7bb8ddd74ac0db08874b2e489e6711173cc5d1cc
awanbadut <diniazik2006@gmail.com>
2025-04-21 00:07:23 +0700

    menambahkan baru

--- WasteWorkApp/resources/views/welcome.blade.php
+++ WasteWorkApp/resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EcoRecycle - Solusi Daur Ulang untuk Masa Depan Lebih Hijau</title>
+    <title>Trash 2 Move- Solusi Daur Ulang untuk Masa Depan Lebih Hijau</title>
 
 </head>
 <body>
